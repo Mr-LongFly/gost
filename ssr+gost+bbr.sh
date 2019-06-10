a4d50555cc1d3683a5ab7cf58c4fde94e6220c55
Mr-LongFly <45267764+Mr-LongFly@users.noreply.github.com>
2019-06-10 16:06:03 +0800

    Update ssr+gost+bbr.sh

--- ssr+gost+bbr.sh
+++ ssr+gost+bbr.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 echo "请提前安装好wget命令，手动安装 CentOS: yum install wget -y , Debian/Ubuntu: apt-get install wget -y !"
-wget -N --no-check-certificate https://github.com/Mr-LongFly/gost/raw/master/ssrmu.sh && chmod +x ssrmu.sh 
+wget -N --no-check-certificate https://github.com/Mr-LongFly/gost/raw/master/ssrmu.sh
 #
-wget -N --no-check-certificate https://github.com/Mr-LongFly/gost/raw/master/gost.sh 
+wget -N --no-check-certificate https://github.com/Mr-LongFly/gost/raw/master/gost.sh
 #
-wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh 
+wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh"
 #
+chmod +x ssrmu.sh tcp.sh
